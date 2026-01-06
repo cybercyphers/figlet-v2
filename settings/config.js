69aeccce392629cd634873cccd6050574eaae72f
cyber <193218221+cybercyphers@users.noreply.github.com>
2026-01-06 21:19:45 +0000

    Add files via upload

---
+++ settings/config.js
@@ -0,0 +1,6 @@
+
+const fs = require('fs')
+global.owner = "233539738956" //owner number
+global.footer = "cypher" //footer section
+global.namabot = "cyphers.js" //nama bot
+global.status = "self"//"self/public" section of the bot
