2895316eda019395adf2632471da11b84699c78e
cyber <193218221+cybercyphers@users.noreply.github.com>
2026-01-08 08:44:02 +0000

    Update config.js

--- settings/config.js
+++ settings/config.js
@@ -4,3 +4,4 @@ global.owner = "233539738956" //owner number
 global.footer = "cypher" //footer section
 global.namabot = "cyphers.js" //nama bot
 global.status = "self"//"self/public" section of the bot
+antidelete:true
